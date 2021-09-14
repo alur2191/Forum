320da737c4a1ba7e99ef467ab0e8fe5f6c7634c8
dandavisjs <dandavisjs@protonmail.com>
2021-09-13 23:28:46 -0700

    Redux; posts actions

---
+++ client/src/actions/post.js
@@ -0,0 +1,85 @@
+import api from '../utils/api';
+import { setAlert } from './alert';
+import {
+    GET_POSTS,
+    POST_ERROR,
+    DELETE_POST,
+    ADD_POST,
+    GET_POST
+} from './types';
+
+// Get posts
+export const getPosts = () => async dispatch => {
+    try {
+        const res = await api.get('/posts');
+
+        dispatch({
+        type: GET_POSTS,
+        payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+    };
+
+
+    // Delete post
+    export const deletePost = id => async dispatch => {
+    try {
+        await api.delete(`/posts/${id}`);
+
+        dispatch({
+        type: DELETE_POST,
+        payload: id
+        });
+
+        dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+        dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+    };
+
+    // Add post
+    export const addPost = formData => async dispatch => {
+    try {
+        const res = await api.post('/posts', formData);
+
+        dispatch({
+        type: ADD_POST,
+        payload: res.data
+        });
+
+        dispatch(setAlert('Post Created', 'success'));
+    } catch (err) {
+        dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+    };
+
+    // Get post
+    export const getPost = id => async dispatch => {
+    try {
+        const res = await api.get(`/posts/${id}`);
+
+        dispatch({
+        type: GET_POST,
+        payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+    };
+
+    
+};
