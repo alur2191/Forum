3ebcd92f05e4b86dac6467652df8dba9544d416d
dandavisjs <dandavisjs@protonmail.com>
2021-09-25 05:49:54 -0700

    Single post page UI - post body, comment textarea

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { getPost } from '../../actions/post';
 import Description from '../category/Description'
 import { ArrowUp,ArrowDown } from 'react-feather';
+import { Bold, Italic, Link as LinkIcon, Code} from 'react-feather';
 
 import api from '../../api/api'
 
@@ -24,23 +25,39 @@ const Post = ({ getPost, post: { post,loading  } }) => {
         <span>LOADING</span>
     ) : (
         <Fragment>
-            <div className="main-body" style={{display:'grid', gridTemplateColumns:'40px 1fr', gridGap:'10px'}}>
-                
-                <div style={{backgroundColor:'#f0f2ff', display: 'flex', flexDirection:'column', alignItems:'center', paddingTop:8}}>
-                    
-                    <ArrowUp/>
-                    •
-                    <ArrowDown/>
+            <div style={{display:'flex', flexDirection:'column', gap: 15}}>
+                <div className="main-body" style={{display:'grid', gridTemplateColumns:'40px 1fr', gridGap:'10px'}}>
+                    <div style={{backgroundColor:'#f0f2ff', display: 'flex', flexDirection:'column', alignItems:'center', paddingTop:8}}>
+                        
+                        <ArrowUp/>
+                        •
+                        <ArrowDown/>
+                    </div>
+                    <div>
+                        <div style={{fontSize:'0.9rem', color:'#4c4d52'}}>Posted by testUser • 4 hours ago</div>
+                            <h3><Link to={`/posts/${id}`}>{post.title}</Link></h3>
+                            <p>{post.body}</p>
+                            <div>
+                                <a>Comments</a>
+                                <a>Share</a>
+                                <a>Save</a>
+                            </div>
+                    </div>
                 </div>
-                <div>
-                    <div style={{fontSize:'0.9rem', color:'#4c4d52'}}>Posted by testUser • 4 hours ago</div>
-                        <h3><Link to={`/posts/${id}`}>{post.title}</Link></h3>
-                        <p>{post.body}</p>
+                <form >
+                    <textarea style={{display: 'flex', minHeight: 80,  width:'100%',padding:0, height: 120,borderBottomLeftRadius:0, borderBottomRightRadius:0}} name="comment-textarea" id="comment-textarea"></textarea>
+                    <div className="comment-buttons">
                         <div>
-                            <a>Comments</a>
-                            <a>Share</a>
-                            <a>Save</a>
+                            <Bold />
+                            <Italic />
+                            <LinkIcon />
+                            <Code />
                         </div>
+                        <input type="submit" value="Comment" />
+                    </div>
+                </form>
+                <div>
+                    <hr color="#E4E8FF" size="1"/>
                 </div>
             </div>
             <div  className="sidebar"><Description/></div>
